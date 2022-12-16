ae10fb102431cbc78b52d4e8a2a0ad8bcc9eaf3c
Martina Keunecke Beck <69729126+martinakbeck@users.noreply.github.com>
2022-12-16 16:47:31 -0300

    Add files via upload

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -13,11 +13,12 @@ st.set_page_config(
     layout='wide'
 )
 
+
 # ======================================
 ### Import Dataframe
 # ======================================
 
-df = pd.read_csv("repos/zomato.csv")
+df = pd.read_csv("../repos/zomato.csv")
 df1 = clean_code(df)
 
 # ======================================
@@ -73,114 +74,12 @@ st.sidebar.markdown('### Powered by Comunidade DS')
 
 st.markdown('### Encontre o seu Restaurante preferido!')
 
-
-
     
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["🏙️ Visão Cidades", "🌍 Visão Países", "🍽️ Visão de Tipos Culinários", "🗺️ Mapa", "🎲 Números" ])
-
-
-with tab5:
-    with st.container():
-        st.markdown('### Melhores Restaurantes dos Princpais tipos Culinários')
-        col1, col2, col3, col4, col5 = st.columns(5)
-
-        with col1:
-            nome, nota = media_culinaria(df1, 'Italian')
-            col1.metric(label=f'Italiana: {nome}', value=f'{nota}/5.0')          
-
-        with col2:
-            nome, nota = media_culinaria(df1, 'American')
-            col2.metric(label=f'Americana: {nome}', value=f'{nota}/5.0')
-
-        with col3:
-            nome, nota = media_culinaria(df1, 'Japanese')
-            col3.metric(label=f'Japanese: {nome}', value=f'{nota}/5.0')
-
-        with col4:
-            nome, nota = media_culinaria(df1, 'Indian')
-            col4.metric(label=f'Indiana: {nome}', value=f'{nota}/5.0')
-
-        with col5:
-            nome, nota = media_culinaria(df1, 'Brazilian')
-            col5.metric(label=f'Brasileira: {nome}', value=f'{nota}/5.0')    
-    
-    st.markdown("""---""")
-    with st.container():
-        st.markdown('### Números da plataforma')
-        col1, col2, col3, col4 = st.columns(4)
-
-        restaurantes_unicos = metricas_gerais(df1, 'restaurant_id')
-        col1.metric('Restaurantes Cadastrados', restaurantes_unicos)
-
-        paises_unicos = metricas_gerais(df1, 'country_code')
-        col2.metric('Países Cadastrados', paises_unicos)
-
-        cidades_unicas = metricas_gerais(df1, 'city')
-        col3.metric('Cidades Cadastradas', cidades_unicas)
-
-        tipos_culinarios = metricas_gerais(df1, 'cuisines')
-        col4.metric('Tipos de Culinárias', tipos_culinarios)
-
-
-
-with tab4:   
-
-    st.markdown( '## Mapa' )
-    with st.spinner('Carregando mapa...'):
-        data_plot = df1.loc[:, ['city', 'restaurant_name', 'longitude', 'latitude']].reset_index()
-        # Desenhar o mapa
-        map_ = folium.Map( zoom_start=11 )
-        cluster = MarkerCluster().add_to(map_)
-
-        for index, location_info in data_plot.iterrows():
-            folium.Marker( [location_info['latitude'],
-                location_info['longitude']],
-                popup=location_info['restaurant_name'], icon= folium.Icon(color='lightgray', icon='home', prefix='fa') ).add_to( cluster )
-
-        folium_static(map_)    
-        
-with tab2:
-    with st.container():
-        df_aux = (df1.loc[:, ['restaurant_id', 'country_code']]
-                  .groupby('country_code')
-                  .count()
-                  .sort_values(['restaurant_id','country_code'], ascending = False)
-                  .reset_index())
-
-        fig = (px.bar(df_aux, x='country_code', y='restaurant_id', 
-                      text='restaurant_id', 
-                      labels={'restaurant_id': 'Quantidade de Restaurantes', 'country_code': 'Países'}, 
-                      title='Quatidade de Restaurantes por País'))
-        st.plotly_chart(fig, use_container_width=True)
-
-    with st.container():
-        df_sem_cidade_duplicada = df1.drop_duplicates('city')
-        df1_cidade_pais = (df_sem_cidade_duplicada.loc[:, ['city', 'country_code']]
-                        .groupby('country_code')
-                        .count()
-                        .sort_values('city', ascending=False)
-                        .reset_index())
-
-        fig = (px.bar(df1_cidade_pais, x='country_code', y='city', 
-                      text='city', 
-                      labels={'city': 'Quantidade de Cidades', 'country_code': 'Países'}, 
-                      title='Quatidade de Cidade por País'))
-
-        st.plotly_chart(fig, use_container_width=True)
-
-    with st.container():
-        col1, col2 = st.columns(2)
-
-        with col1:
-
-            fig = grafico_media_pais(df1, 'votes')        
-            st.plotly_chart(fig, use_container_width=True)        
-
-        with col2:
-            fig = grafico_media_pais(df1, 'average_cost_for_two')        
-            st.plotly_chart(fig, use_container_width=True) 
-    
-    
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["🏙️ Visão Cidades", "🌍 Visão Países", "🍽️ Visão de Tipos Culinários", "🗺️ Mapa", "🎲 Métricas Gerais" ])
+   
+#===========================
+# VISÃO CIDADES
+#==========================
 with tab1:
     top_rest = st.slider('Quantidade de restaurantes:', 0, 20, 10)  
 
@@ -258,7 +157,55 @@ with tab1:
 
         st.plotly_chart(fig, use_container_width=True) 
 
-    
+#===========================
+# VISÃO PAÍSES
+#==========================
+        
+with tab2:
+    with st.container():
+        df_aux = (df1.loc[:, ['restaurant_id', 'country_code']]
+                  .groupby('country_code')
+                  .count()
+                  .sort_values(['restaurant_id','country_code'], ascending = False)
+                  .reset_index())
+
+        fig = (px.bar(df_aux, x='country_code', y='restaurant_id', 
+                      text='restaurant_id', 
+                      labels={'restaurant_id': 'Quantidade de Restaurantes', 'country_code': 'Países'}, 
+                      title='Quatidade de Restaurantes por País'))
+        st.plotly_chart(fig, use_container_width=True)
+
+    with st.container():
+        df_sem_cidade_duplicada = df1.drop_duplicates('city')
+        df1_cidade_pais = (df_sem_cidade_duplicada.loc[:, ['city', 'country_code']]
+                        .groupby('country_code')
+                        .count()
+                        .sort_values('city', ascending=False)
+                        .reset_index())
+
+        fig = (px.bar(df1_cidade_pais, x='country_code', y='city', 
+                      text='city', 
+                      labels={'city': 'Quantidade de Cidades', 'country_code': 'Países'}, 
+                      title='Quatidade de Cidade por País'))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with st.container():
+        col1, col2 = st.columns(2)
+
+        with col1:
+
+            fig = grafico_media_pais(df1, 'votes')        
+            st.plotly_chart(fig, use_container_width=True)        
+
+        with col2:
+            fig = grafico_media_pais(df1, 'average_cost_for_two')        
+            st.plotly_chart(fig, use_container_width=True) 
+
+            
+#===========================
+# VISÃO TIPOS CULINÁRIOS
+#==========================
 with tab3:
     col1_filtro, col2_filtro = st.columns(2)
     with col1_filtro:
@@ -275,12 +222,6 @@ with tab3:
         #========== Ligar Filtro =============
         linhas_selecionadas = df1['cuisines'].isin(selected_cuisines)
         df1 = df1.loc[linhas_selecionadas, :]
-
-
-
-
-
-
 
     with st.container():
 
@@ -309,11 +250,72 @@ with tab3:
         with st.expander("Clique para expandir"):
             st.write(f'Top {top_rest} Restaurantes') 
             st.dataframe(df_top)
+            
+#===========================
+# MAPA
+#==========================            
+            
+with tab4:   
 
+    st.markdown( '## Mapa' )
+    with st.spinner('Carregando mapa...'):
+        data_plot = df1.loc[:, ['city', 'restaurant_name', 'longitude', 'latitude']].reset_index()
+        # Desenhar o mapa
+        map_ = folium.Map( zoom_start=11 )
+        cluster = MarkerCluster().add_to(map_)
 
-        
+        for index, location_info in data_plot.iterrows():
+            folium.Marker( [location_info['latitude'],
+                location_info['longitude']],
+                popup=location_info['restaurant_name'], icon= folium.Icon(color='lightgray', icon='home', prefix='fa') ).add_to( cluster )
 
+        folium_static(map_) 
             
 
-
+#===========================
+# NÚMEROS
+#==========================
     
+with tab5:
+    with st.container():
+        st.markdown('### Melhores Restaurantes dos Princpais tipos Culinários')
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            nome, nota = media_culinaria(df1, 'Italian')
+            col1.metric(label=f'Italiana: {nome}', value=f'{nota}/5.0')          
+
+        with col2:
+            nome, nota = media_culinaria(df1, 'American')
+            col2.metric(label=f'Americana: {nome}', value=f'{nota}/5.0')
+
+        with col3:
+            nome, nota = media_culinaria(df1, 'Japanese')
+            col3.metric(label=f'Japanese: {nome}', value=f'{nota}/5.0')
+
+        with col4:
+            nome, nota = media_culinaria(df1, 'Indian')
+            col4.metric(label=f'Indiana: {nome}', value=f'{nota}/5.0')
+
+        with col5:
+            nome, nota = media_culinaria(df1, 'Brazilian')
+            col5.metric(label=f'Brasileira: {nome}', value=f'{nota}/5.0')    
+    
+    st.markdown("""---""")
+    with st.container():
+        st.markdown('### Números da plataforma')
+        col1, col2, col3, col4 = st.columns(4)
+
+        restaurantes_unicos = metricas_gerais(df1, 'restaurant_id')
+        col1.metric('Restaurantes Cadastrados', restaurantes_unicos)
+
+        paises_unicos = metricas_gerais(df1, 'country_code')
+        col2.metric('Países Cadastrados', paises_unicos)
+
+        cidades_unicas = metricas_gerais(df1, 'city')
+        col3.metric('Cidades Cadastradas', cidades_unicas)
+
+        tipos_culinarios = metricas_gerais(df1, 'cuisines')
+        col4.metric('Tipos de Culinárias', tipos_culinarios)
+
+
